84766ebd592fb9dfb6d82c91af7219e4970e561d
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-12 22:06:24 -0600

    commit tree 23dab4a7131bd29ce7c4c5cba5f154a85371a0ee

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/new' do
+        erb :create_puppy
+    end
+    
+    #method should be last -if others exist
+    get '/' do
+        erb :index
+    end
+
+    post '/puppy' do
+        @puppy = params
+        erb :display_puppy
+    end
+
 end
